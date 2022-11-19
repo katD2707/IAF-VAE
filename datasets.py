2b872f4bb6ebe91dccbb54187712e35b13154e8c
dat <dat27072002@gmail.com>
2022-11-19 09:13:23 +0700

    add dataset

--- datasets.py
+++ datasets.py
@@ -50,3 +50,52 @@ class Cifar10:
             return train_loader, test_loader
 
         return train_loader
+
+
+class Mnist:
+    def __init__(self,
+                 train,
+                 val,
+                 transform_train=None,
+                 transform_val=None,
+                 root='./data',
+                 download=True,
+                 ):
+        self.trainset = datasets.MNIST(root=root,
+                                       train=True,
+                                       download=download,
+                                       transform=transform_train,
+                                       )
+        if val:
+            self.testset = datasets.MNIST(root=root,
+                                          train=False,
+                                          download=download,
+                                          transform=transform_val,
+                                          )
+        self.val = val
+
+    def get_dataloader(self,
+                       batch_size,
+                       shuffle=True,
+                       drop_last=True,
+                       num_workers=2,
+                       pin_memory=True,
+                       ):
+        train_loader = torch.utils.data.DataLoader(self.trainset,
+                                                   batch_size=batch_size,
+                                                   shuffle=shuffle,
+                                                   num_workers=num_workers,
+                                                   drop_last=drop_last,
+                                                   pin_memory=pin_memory,
+                                                   )
+        if self.val:
+            test_loader = torch.utils.data.DataLoader(self.testset,
+                                                      batch_size=batch_size,
+                                                      shuffle=shuffle,
+                                                      num_workers=num_workers,
+                                                      drop_last=drop_last,
+                                                      pin_memory=pin_memory,
+                                                      )
+            return train_loader, test_loader
+
+        return train_loader
