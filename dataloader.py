9db008e328bd355869851dee6006c6d9b2d059b2
dat <dat27072002@gmail.com>
2022-11-24 03:05:40 +0700

    debug

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,6 @@
 import torch.utils.data
-import torchvision.datasets as datasets
-
+# import torchvision.datasets as datasets
+import datasets
 
 class Cifar10:
     def __init__(self,
@@ -12,13 +12,13 @@ class Cifar10:
                  download=True,
                  ):
         self.trainset = datasets.CIFAR10(root=root,
-                                         train=True,
+                                         train=train,
                                          download=download,
                                          transform=transform_train,
                                          )
         if val:
             self.testset = datasets.CIFAR10(root=root,
-                                            train=False,
+                                            train=val,
                                             download=download,
                                             transform=transform_val,
                                             )
@@ -51,53 +51,53 @@ class Cifar10:
         return train_loader
 
 
-class Mnist:
-    def __init__(self,
-                 train,
-                 val,
-                 transform_train=None,
-                 transform_val=None,
-                 root='./data',
-                 download=True,
-                 ):
-        self.trainset = datasets.MNIST(root=root,
-                                       train=True,
-                                       download=download,
-                                       transform=transform_train,
-                                       )
-        if val:
-            self.testset = datasets.MNIST(root=root,
-                                          train=False,
-                                          download=download,
-                                          transform=transform_val,
-                                          )
-        self.val = val
-
-    def get_dataloader(self,
-                       batch_size,
-                       shuffle=True,
-                       drop_last=True,
-                       num_workers=2,
-                       pin_memory=True,
-                       ):
-        train_loader = torch.utils.data.DataLoader(self.trainset,
-                                                   batch_size=batch_size,
-                                                   shuffle=shuffle,
-                                                   num_workers=num_workers,
-                                                   drop_last=drop_last,
-                                                   pin_memory=pin_memory,
-                                                   )
-        if self.val:
-            test_loader = torch.utils.data.DataLoader(self.testset,
-                                                      batch_size=batch_size,
-                                                      shuffle=shuffle,
-                                                      num_workers=num_workers,
-                                                      drop_last=drop_last,
-                                                      pin_memory=pin_memory,
-                                                      )
-            return train_loader, test_loader
-
-        return train_loader
+# class Mnist:
+#     def __init__(self,
+#                  train,
+#                  val,
+#                  transform_train=None,
+#                  transform_val=None,
+#                  root='./data',
+#                  download=True,
+#                  ):
+#         self.trainset = datasets.MNIST(root=root,
+#                                        train=True,
+#                                        download=download,
+#                                        transform=transform_train,
+#                                        )
+#         if val:
+#             self.testset = datasets.MNIST(root=root,
+#                                           train=False,
+#                                           download=download,
+#                                           transform=transform_val,
+#                                           )
+#         self.val = val
+#
+#     def get_dataloader(self,
+#                        batch_size,
+#                        shuffle=True,
+#                        drop_last=True,
+#                        num_workers=2,
+#                        pin_memory=True,
+#                        ):
+#         train_loader = torch.utils.data.DataLoader(self.trainset,
+#                                                    batch_size=batch_size,
+#                                                    shuffle=shuffle,
+#                                                    num_workers=num_workers,
+#                                                    drop_last=drop_last,
+#                                                    pin_memory=pin_memory,
+#                                                    )
+#         if self.val:
+#             test_loader = torch.utils.data.DataLoader(self.testset,
+#                                                       batch_size=batch_size,
+#                                                       shuffle=shuffle,
+#                                                       num_workers=num_workers,
+#                                                       drop_last=drop_last,
+#                                                       pin_memory=pin_memory,
+#                                                       )
+#             return train_loader, test_loader
+#
+#         return train_loader
 
 
 class CelebA:
--- datasets/CelebA.py
+++ datasets/CelebA.py
@@ -48,7 +48,7 @@ class CelebA(VisionDataset):
         # ("0B7EVK8r0v71pbWNEUjJKdDQ3dGc","b6cd7e93bc7a96c2dc33f819aa3ac651", "img_align_celeba_png.7z"),
         # ("0B7EVK8r0v71peklHb0pGdDl6R28", "b6cd7e93bc7a96c2dc33f819aa3ac651", "img_celeba.7z"),
         ("0B7EVK8r0v71pblRyaVFSWGxPY0U", "75e246fa4810816ffd6ee81facbd244c", "list_attr_celeba.txt"),
-        ("1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS", "32bd1bd63d3c78cd57e08160ec5ed1e2", "identity_CelebA.txt"),
+        # ("1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS", "32bd1bd63d3c78cd57e08160ec5ed1e2", "identity_CelebA.txt"),
         ("0B7EVK8r0v71pbThiMVRxWXZ4dU0", "00566efa6fedff7a56946cd1c10f1c16", "list_bbox_celeba.txt"),
         ("0B7EVK8r0v71pd0FJY3Blby1HUTQ", "cc24ecafdb5b50baae59b03474781f8c", "list_landmarks_align_celeba.txt"),
         # ("0B7EVK8r0v71pTzJIdlJWdHczRlU", "063ee6ddb681f96bc9ca28c6febb9d1a", "list_landmarks_celeba.txt"),
